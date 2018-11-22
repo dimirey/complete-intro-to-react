3d0ee8f05952ef86c66b96c6953aa30f42f9cb90
Dimitri Reynaga <dimitri@Dimitris-MacBook-Pro.local>
2018-11-21 17:12:17 -0800

    test

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,37 +2,29 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import type { RouterHistory } from 'react-router-dom';
-import { object } from 'prop-types';
+// import { Link } from 'react-router-dom';
+// import type { RouterHistory } from 'react-router-dom';
+// import { object } from 'prop-types';
 import { setSearchTerm } from './actionCreators';
 
 class Landing extends React.Component {
   static contextTypes = {
-    history: object
+    // history: object
   };
-  props: {
-    searchTerm: string,
-    handleSearchTermChange: Function,
-    history: RouterHistory
-  };
+  // props: {
+  //   searchTerm: string,
+  //   handleSearchTermChange: Function,
+  //   history: RouterHistory
+  // };
   goToSearch = event => {
     event.preventDefault();
-    this.props.history.push('/search');
+    // this.props.history.push('/search');
   };
   render() {
     return (
       <div className="landing">
-        <h1>svideo</h1>
-        <form onSubmit={this.goToSearch}>
-          <input
-            onChange={this.props.handleSearchTermChange}
-            value={this.props.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </form>
-        <Link to="/search">or Browse All</Link>
+        <h1>i love bebisas</h1>
+        {/* <Link to="/search">or Browse All</Link> */}
       </div>
     );
   }
